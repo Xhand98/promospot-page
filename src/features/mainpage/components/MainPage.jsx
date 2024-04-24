6173a719c4bb726f7d6587eec0e5752c4a443ab7
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-04-24 09:58:20 -0400

    Changed directory and added the MainPage

---
+++ src/features/mainpage/components/MainPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './MainPage.css'
+
+function MainPage() {
+  return (
+    <main>
+
+      <div class="parrafo">
+        <p>
+          !Disfruta de todas las ofertas y catálogos que tenemos disponible para
+          ti{" "}
+        </p>
+      <div class="Img">
+        <img src="../assets/fff.png" class="carrito" alt=""/>
+        <img src="../assets/fff.png" class="precio" alt="" />
+      </div>
+      </div>
+    </main>
+  );
+}
+
+export default MainPage;
