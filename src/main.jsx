342ebb95bc7792633954930522cdb460540ee1cf
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-04-30 09:24:01 -0400

    Suddle changes on App and main pages

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <link href="https://unpkg.com/@csstools/normalize.css" rel="stylesheet" />
     <Header />
+    
     <MainPage />
     <App />
 
