01d564e57c89059db6fbc1c0313b6f26fe33431d
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-04-30 08:03:30 -0400

    Fixed the bar on Header

--- src/features/header/components/Header.jsx
+++ src/features/header/components/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faYoutube,  faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import logo from '../assets/logo.png'
     
 
 const Footer = () => {
@@ -19,32 +21,42 @@ const Footer = () => {
         }
     };
 
+    var logo1 = (
+        <img 
+          //src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg"
+          src={logo}
+          alt="Canvas Logo"
+          className='logo'
+        />
+    )
    
 
     return (
-        <header class="main-header">
-    <label for="btn-nav" class="btn-nav"><FontAwesomeIcon icon={ faBars }/></label>
+        <header className="main-header">
+    <label for="btn-nav" className="btn-nav"><FontAwesomeIcon icon={ faBars }/></label>
     <input type="checkbox" id="btn-nav" />
+    { logo1 }
+    <h2 className="pageName">PromoSpot</h2>
 
-    <nav class="navigation">
-        <ul class="menu">
-            <li class="Loterias"><a href="#">Loterias uFUFUFSA</a>
-                <ul class="submenu" >
-                    <li class="li Leidsa">Leidsa</li>
+    <nav className="navigation">
+        <ul className="menu">
+            <li className="Loterias"><a href="#">Loterias uFUFUFSA</a>
+                <ul className="submenu" >
+                    <li className="li Leidsa">Leidsa</li>
                     
                     <li>Lotoreal</li>
                   
                     <li>Loteka</li>
                 </ul>
             </li>
-            <li class="Farmacias"><a href="#">Farmacias</a>
-                <ul class="submenu">
+            <li className="Farmacias"><a href="#">Farmacias</a>
+                <ul className="submenu">
                     <li>Carol</li>
                     <li>Hidalgo</li>
                 </ul>
             </li>
-            <li class="Supermercados"><a href="#">Supermercados</a>
-                <ul class="submenu">
+            <li className="Supermercados"><a href="#">Supermercados</a>
+                <ul className="submenu">
                     <li>Bravo</li>
 
                     <li>Olé</li>
@@ -58,10 +70,16 @@ const Footer = () => {
 
     </nav>
 
-
-    <input type="text" placeholder="¿Que buscas?" class="buscador" id='searchbar' onKeyDown={handleKeyPress} />
-
    
+
+   <div className="searchcont"> 
+  <div className='iconcont'>
+   <input type="text" placeholder="¿Que buscas?" className="buscador" id='searchbar' onKeyDown={handleKeyPress} />
+   <a href=''><i><FontAwesomeIcon icon={ faYoutube }/></i></a>
+   <a href=''><i><FontAwesomeIcon icon={ faInstagram }/></i></a>
+   <a href=''><i><FontAwesomeIcon icon={ faFacebookF }/></i></a>
+   </div>
+   </div>
 </header>
 
     );
