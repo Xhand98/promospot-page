0507eac7c915d95593bf49f9f4c3052f1e5b46cc
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-04-30 14:28:57 -0400

    Change on app/main

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,21 @@ import Footer from './features/footer/components/Footer.jsx';
 import ChatbotToggler from './features/chatbot/components/ChatbotToggler.jsx';
 import Header from './features/header/components/Header.jsx';
 import MainPage from './features/mainpage/components/MainPage.jsx';
+import { BrowserRouter } from "react-router-dom";
+import Navigation from './App.jsx'
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <link href="https://unpkg.com/@csstools/normalize.css" rel="stylesheet" />
     <Header />
     
     <MainPage />
 
-    <Footer />
-    <ChatbotToggler />
-  </React.StrictMode>,
+   <Footer />
+    ////<ChatbotToggler />
+    <Navigation />
+  </BrowserRouter>,
 )
